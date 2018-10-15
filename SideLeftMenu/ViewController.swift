e82728654d41133aeeef97e0435f747a823d74de
Vladimir Matukh <vladimir@app3null.com>
2018-10-16 02:46:18 +0300

    Initial Commit

---
+++ SideLeftMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SideLeftMenu
+//
+//  Created by Volodymyr Matukh on 10/16/18.
+//  Copyright © 2018 ForMe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
